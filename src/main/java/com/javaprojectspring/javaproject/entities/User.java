18382a30e992f3a0e144443fce808c8ad87dae04
giovani <giovani.eugenio.sp@gmail.com>
2024-04-17 12:01:34 -0300

    feat: insert user

--- src/main/java/com/javaprojectspring/javaproject/entities/User.java
+++ src/main/java/com/javaprojectspring/javaproject/entities/User.java
@@ -26,7 +26,7 @@ public class User implements Serializable{
 	
 	private String name;
 	private String email;
-	private String fone;
+	private String phone;
 	private String password;
 	
 	@JsonIgnore  //just client data, no orders
@@ -36,11 +36,11 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(Long id, String name, String email, String fone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.fone = fone;
+		this.phone = phone;
 		this.password = password;
 	}
 
@@ -68,12 +68,12 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public String getFone() {
-		return fone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setFone(String fone) {
-		this.fone = fone;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public String getPassword() {
